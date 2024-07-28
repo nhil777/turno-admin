21670d88a613dbfdd36aab637b5385f18a928c38
Yan Bein <yanbeindev@gmail.com>
2024-07-28 11:02:09 -0300

    feat(core): install font awesome

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,11 @@
+import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Login = () => {
     return (
         <>
             <p>Login</p>
+            <FontAwesomeIcon icon={faDog} />
         </>
     );
 };
