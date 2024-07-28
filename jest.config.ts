563223649db726f14e840bdaf03d7394bebbcecf
Yan Bein <yanbeindev@gmail.com>
2024-07-28 19:06:41 -0300

    fix(jest): Fix file mock

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,6 @@ export default {
         "^.+\\.tsx?$": "ts-jest"
     },
     moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     },
 }
