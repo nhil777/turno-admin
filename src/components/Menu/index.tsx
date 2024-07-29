f7b6314d5b65393a945002d7e91c152681f337fd
Yan Bein <yanbeindev@gmail.com>
2024-07-29 17:23:41 -0300

    feat(header): create menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import { Nav, Offcanvas } from "react-bootstrap";
+import { MenuProps } from "./types";
+
+export const Menu = ({ show, onHide }: MenuProps) => {
+    return (
+        <Offcanvas show={show} onHide={onHide}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Nav className="flex-column">
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav>
+            </Offcanvas.Body>
+        </Offcanvas>
+    );
+}
